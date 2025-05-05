7629628bdec49f07cc15f81aebdfb46e81a1449f
sirishakoduru <sravanthi.tej@gmail.com>
2025-05-05 10:29:24 -0400

    initial commit

---
+++ src/test/java/utilities/ReceipeDatabaseOperations.java
@@ -0,0 +1,20 @@
+package utilities;
+
+import java.sql.PreparedStatement;
+
+public class ReceipeDatabaseOperations {
+	
+//	public void createTable() throws Exception
+//	{
+//		try {
+//			String createTableReceipequery="CREATE TABLE public.Receipes (Recipe_ID,Recipe_Name,Recipe_Category, Food_Category,Ingredients,Preparation_Time,Cooking_Time,Tag,No_of_servings,Cuisine_category,Recipe_Description,Preparation_method,Nutrient_values,Recipe_URL);";
+//			PreparedStatement ps = null;
+//			ps = getConn().prepareStatement(createTableReceipequery);
+//			ps.execute();
+//			System.out.println("Table Created...");
+//		} catch (Exception e) {	
+//			e.printStackTrace();
+//		}
+//	}
+
+}
